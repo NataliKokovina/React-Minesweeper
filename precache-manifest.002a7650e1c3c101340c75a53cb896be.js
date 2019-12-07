1a154f0a15035059c6ffe71a943f8e1336dd0982
Nataliya Kokovina <kokovina.nat>
2019-12-07 16:07:30 +0200

    Updates

---
+++ precache-manifest.002a7650e1c3c101340c75a53cb896be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "640951388e080c33038d7d8ddd1b98db",
+    "url": "/React-Minesweeper/index.html"
+  },
+  {
+    "revision": "37746783c3ef487d400d",
+    "url": "/React-Minesweeper/static/css/main.ce4221d1.chunk.css"
+  },
+  {
+    "revision": "2bef55b4905266ee6e77",
+    "url": "/React-Minesweeper/static/js/2.aa8e4a11.chunk.js"
+  },
+  {
+    "revision": "37746783c3ef487d400d",
+    "url": "/React-Minesweeper/static/js/main.f69b7e53.chunk.js"
+  },
+  {
+    "revision": "f7cf7e0ea3ad2abf0d2e",
+    "url": "/React-Minesweeper/static/js/runtime-main.0665b507.js"
+  }
+]);
